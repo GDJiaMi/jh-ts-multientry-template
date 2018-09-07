33df60433a4c42d647408049b5f49af379e9e5a5
carney520 <carney520@hotmail.com>
2018-09-07 10:37:10 +0800

    移除clean-webpack-plugin
    # Conflicts:
    #	internals/config.js

--- internals/config.js
+++ internals/config.js
@@ -4,7 +4,6 @@
 const webpack = require('webpack')
 const glob = require('glob')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
 const tsImportPluginFactory = require('ts-import-plugin')
 const path = require('path').posix
 const getEnviroment = require('./env')
@@ -179,9 +178,6 @@ module.exports = (env, argv) => {
         ...(envConfig.optimization || {}),
       },
       plugins: [
-        new CleanWebpackPlugin('dist', {
-          root,
-        }),
         // 移除moment语言包
         new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
         new webpack.DefinePlugin(enviroments.stringified),
